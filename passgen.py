d734ca929c97fae22a045ec186a7216253c1065b
KokoT <71900420+imKokoT@users.noreply.github.com>
2025-07-05 22:07:47 +0200

    just small fixes, docs

--- passgen.py
+++ passgen.py
@@ -31,13 +31,12 @@ import string
 VERSION = 2
 
 # --- SETTINGS ------------------------------------------------------------------------------------------
-DEFAULT_LENGTH = 24         # default password length; default 24
-COPY_TO_CLIPBOARD = True    # If True and run as app, it will copy password to clipboard; default True
-SYMBOLS_CHARSET = '!@#$%^&*()-_=+[]{}<>?'
-DEFAULT_CHARSET = string.ascii_letters + string.digits + SYMBOLS_CHARSET
-# -------------------------------------------------------------------------------------------------------
+DEFAULT_LENGTH = 24                        # default password length; default 24
+COPY_TO_CLIPBOARD = True                   # if True and run as app, it will copy password to clipboard; default True
+SYMBOLS_CHARSET = '!@#$%^&*()-_=+[]{}<>?'  # charset of default ascii symbols; key `s` -------------------------------------------------------------------------------------------------------
 
 DEFAULT_FORMAT = f'{{{DEFAULT_LENGTH}}}'
+DEFAULT_CHARSET = string.ascii_letters + string.digits + SYMBOLS_CHARSET
 ALPHABET_LOWER_CHARSET = string.ascii_lowercase
 ALPHABET_UPPER_CHARSET = string.ascii_uppercase
 HEX_LOWER_CHARSET = string.hexdigits.replace('ABCDEF', '')
@@ -45,10 +44,6 @@ HEX_UPPER_CHARSET = string.hexdigits.replace('abcdef', '')
 DIGITS_CHARSET = string.digits
 ALPHADIGITS_CHARSET = string.ascii_letters + string.digits
 PLACEHOLDER_PATTERN = r'(?<!\\){([a-zA-Z0-9\s]*)}'
-
-
-def __rep(match):
-    return match.group(1).strip()
 
 
 def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
@@ -76,6 +71,9 @@ def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
             - x -> lower hex
             - s -> symbols from `SYMBOLS_CHARSET`
             - c[] -> your symbols in `[]`; `\\[` `\\]` to use them as target symbols. duplicates will be removed
+    
+    Raises:
+        ValueError: if wrong format
     '''
     placeholdersRules = re.findall(PLACEHOLDER_PATTERN, format.replace('\\{', '\ufff0').replace('\\}', '\ufff1'))
     for p in placeholdersRules:
@@ -95,18 +93,21 @@ def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
         charset = set()
         for rule in rules[1:]:
             match rule:
-                case 'A': charset.update(ALPHABET_UPPER_CHARSET)  
-                case 'a': charset.update(ALPHABET_LOWER_CHARSET)  
-                case 'd': charset.update(DIGITS_CHARSET)  
-                case 'X': charset.update(HEX_UPPER_CHARSET)  
-                case 'x': charset.update(HEX_LOWER_CHARSET)  
-                case 's': charset.update(SYMBOLS_CHARSET)  
+                case 'A': charset.update(ALPHABET_UPPER_CHARSET)
+                case 'a': charset.update(ALPHABET_LOWER_CHARSET)
+                case 'd': charset.update(DIGITS_CHARSET)
+                case 'X': charset.update(HEX_UPPER_CHARSET)
+                case 'x': charset.update(HEX_LOWER_CHARSET)
+                case 's': charset.update(SYMBOLS_CHARSET)
                 case 'c': raise NotImplementedError('soon')
-         
-        format = re.sub(
-            PLACEHOLDER_PATTERN,
-            lambda m: ''.join(secrets.choice(tuple(charset)) for _ in range(length)),
-            format, count=1)
+        
+            if not charset:
+                raise ValueError(f'charset of {{{p}}} is empty!')
+
+            format = re.sub(
+                PLACEHOLDER_PATTERN,
+                lambda m: ''.join(secrets.choice(tuple(charset)) for _ in range(length)),
+                format, count=1)
         
     return format.replace('\\{', '{').replace('\\}', '}')
 
@@ -117,7 +118,7 @@ if __name__ == "__main__":
         import clipboard
     except ModuleNotFoundError:
         COPY_TO_CLIPBOARD = False
-        print(f'{COPY_TO_CLIPBOARD=} because you don\'t install "clipboard" module to copy. please pip install clipboard')
+        print(f'{COPY_TO_CLIPBOARD=} because you didn\'t install "clipboard" module to copy. please pip install clipboard')
 
     parser = argparse.ArgumentParser(description=f'passgen v{VERSION} for secure password generating by imKokoT using secrets module! see https://github.com/imKokoT')
     parser.add_argument('-f', '--format', type=str, default=DEFAULT_FORMAT, help=f'format of password; default "{DEFAULT_FORMAT}"', required=False)
