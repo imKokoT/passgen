1712c7f676a51edc72ea99d1f201af8719fb24e3
KokoT <71900420+imKokoT@users.noreply.github.com>
2025-07-05 23:23:20 +0200

    v2 finished
    - supports formating with different build-in charsets or your's
    - simple import to your project
    - if run as app, possible to set format and generate many passwords to choose the best one

--- passgen.py
+++ passgen.py
@@ -45,7 +45,7 @@ DIGITS_CHARSET = string.digits
 ALPHADIGITS_CHARSET = string.ascii_letters + string.digits
 # i just hate regex
 PLACEHOLDER_PATTERN = re.compile(r'(?<!\\){([^{}]*)}')
-CUSTOM_PATTERN = re.compile(r'c(?<!\\)\[((?:[^\]\\]|\\.)*?)\](?!\])')
+CUSTOM_PATTERN = re.compile(r'(?<!\\)\[((?:[^\]\\]|\\.)*?)\](?!\])')
 
 
 def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
@@ -57,7 +57,9 @@ def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
 
             generated chars replaces instead of `{}`. you can write rule in it to format output result. 
             you can use any other symbols out of `{}` to make it different format. use `\\{` and `\\}` to 
-            place braces out of placeholder. number in braces describes how many symbols to generate.
+            place braces out of placeholder, in placeholder \\{ and \\} don't work. number in braces 
+            describes how many symbols to generate. if format is wrong, will raise ValueError or handle it 
+            as static string.
 
             for example `{16}` returns password of `DEFAULT_CHARSET` symbols with length 16.
 
@@ -65,14 +67,15 @@ def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
             `{8 x}-{4 x}-{4 x}-{4 x}-{12 x}` returns UUID like format in lowercase.
 
             ### charsets
-            you can combine charsets. duplicates don't affect to probability. example `{16 A a d c[-+$%=\\[\\]/]}`
+            you can combine charsets. duplicates don't affect to probability. example `{16 A a d [-+$%=\\[\\]/]}`
             - A -> ascii upper letters
             - a -> ascii lower letters
             - d -> digits
             - X -> upper hex
             - x -> lower hex
             - s -> symbols from `SYMBOLS_CHARSET`
-            - c[] -> your symbols in `[]`; `\\[` `\\]` to use them as target symbols.
+            - b -> `{}`
+            - [] -> your symbols in `[]`; `\\[` `\\]` to use them as target symbols.
     
     Raises:
         ValueError: if wrong format
@@ -101,6 +104,7 @@ def passgen(format:str=f'{{{DEFAULT_LENGTH}}}') -> str:
                 case 'X': charset.update(HEX_UPPER_CHARSET)
                 case 'x': charset.update(HEX_LOWER_CHARSET)
                 case 's': charset.update(SYMBOLS_CHARSET)
+                case 'b': charset.update('{}') # because i hate regex
                 case _:
                     if not re.match(CUSTOM_PATTERN, rule): continue
                     charset.update(
@@ -128,8 +132,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=f'passgen v{VERSION} for secure password generating by imKokoT using secrets module! see https://github.com/imKokoT')
     parser.add_argument('-f', '--format', type=str, default=DEFAULT_FORMAT, help=f'format of password; default "{DEFAULT_FORMAT}"', required=False)
-    args = parser.parse_args()
+    parser.add_argument('-c', '--count', type=int, default=1, help=f'how many passwords to generate', required=False)
 
-    pwd = passgen(args.format)
-    print(pwd)
+    args = parser.parse_args()
+    pwds = ''
+    while args.count:
+        pwd = passgen(args.format)
+        print(pwd)
+        pwds += pwd + '\n'
+        args.count -= 1
     if COPY_TO_CLIPBOARD: clipboard.copy(pwd)
